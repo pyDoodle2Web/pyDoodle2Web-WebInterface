f4b66a778f5b47060771c7f56114bf5a5a16b4fe
= <aayushdongre4@gmail.com>
2020-03-22 22:34:25 +0530

    source code download option

--- pyD2W/urls.py
+++ pyD2W/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('upload/', views.upload, name='upload'),
+    path('generated/', views.generated, name='generated'),
+    path('downloadSource/', views.downloadSource, name='downloadSource')
 ]
--- pyD2W/views.py
+++ pyD2W/views.py
@@ -1,9 +1,14 @@
 
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+from django.utils.encoding import smart_str
+from django.http import HttpResponse
+from django.http import FileResponse
 from .ocr import OCR
 from django.core.validators import validate_image_file_extension
 from generator.main import HTMLGenerator
+import os
+
 
 def home(request):
     return render(request, 'home.html')
@@ -20,9 +25,23 @@ def upload(request):
             context['url'] = fs.url(name)
             print(name)
             tagsList = OCR(name).readText()
-            soup, _ = HTMLGenerator(tagsList).generateHTML()
-
+            html, _ = HTMLGenerator(tagsList).generateHTML()
+            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates', 'generated.html'), 'w') as f:
+                f.flush()
+                f.write(str(html.template.prettify()))
+            context['generated_url'] = True
         except Exception:
             context['error'] = 'Error Occurred! Make sure the uploaded file is an Image'
-        
+
     return render(request, 'upload.html', context)
+
+
+def generated(request):
+    return render(request, 'generated.html')
+
+
+def downloadSource(request):
+    fs = FileSystemStorage(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
+    response = FileResponse(fs.open('generated.html', 'rb'), content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str('generated.html')
+    return response
