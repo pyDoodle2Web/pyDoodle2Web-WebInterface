7699ff4f1c2ae2ff1986941cd16bf9ccaa3a24cc
uniyalabhishek <56363630+uniyalabhishek@users.noreply.github.com>
2020-03-26 23:28:34 +0530

    required changes has been completed.

--- components/footer/footer.py
+++ components/footer/footer.py
@@ -3,9 +3,9 @@ import os
 
 class Footer:
     def __init__(self):
-        self.name = 'Footer'
+        self.name = 'footer'
         self.isParentLike = False
 
-        with open(os.path.join(os.getcwd(), 'components', 'footer', 'template.html')) as f:
+        with open(os.path.join(os.realpath(footer.py), 'components', 'footer', 'template.html')) as f:
             soup = BeautifulSoup(f, 'html.parser')
         self.template = soup
