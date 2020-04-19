aa3a5362104a86e77502757da5b3220784b4e0c1
= <aayushdongre4@gmail.com>
2020-04-19 08:13:21 +0530

    Landing Page

--- pyD2W/templates/frontend/src/App.js
+++ pyD2W/templates/frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from './screens/LandingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 const App = () => (
   <BrowserRouter>
