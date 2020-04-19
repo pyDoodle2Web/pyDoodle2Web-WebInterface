e32963e04b0bfe628c3424e168bcb6d0891be085
= <aayushdongre4@gmail.com>
2020-04-19 07:55:23 +0530

    Setup react router

--- pyD2W/templates/frontend/src/App.js
+++ pyD2W/templates/frontend/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LandingPage from './screens/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={LandingPage} />
+    </Switch>
+  </BrowserRouter>
+
+);
 
 export default App;
---
+++ pyD2W/templates/frontend/src/screens/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+}
+
+export default LandingPage;
