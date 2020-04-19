10b923d7627e3c7a0c95e2615e65c04fbccc9671
= <aayushdongre4@gmail.com>
2020-04-19 11:44:57 +0530

    conditionally rendering upload form

--- pyD2W/frontend/src/components/UploadForm.js
+++ pyD2W/frontend/src/components/UploadForm.js
@@ -1,68 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const UploadForm = () => {
+    const [formState, setFormState] = useState({
+        carousel: false,
+        navbar: false,
+        upload: false
+    });
+    const [tags, setTags] = useState([])
+    useEffect(() => {
+        if (tags.length != 0) {
+            setFormState({ ...formState, upload: true })
+        }
+        if (tags.includes('navbar')) {
+            setFormState({ ...formState, navbar: true })
+        }
+        if (tags.includes('carousel')) {
+            setFormState({ ...formState, 'carousel': true })
+        }
+    }, [tags])
+
     return (
-        <form className="form-group mt-4"  id="form" onSubmit={submitHandler} >
+        <form
+            className="form-group mt-4"
+            id="form"
+            onSubmit={async (e) => {
+                e.preventDefault();
+                const form = document.getElementById('form');
+                const data = new FormData(form);
+                fetch('/readImage/', {
+                    method: 'POST',
+                    body: data,
+                    headers: {
+                        "X-CSRFToken": getCoockie('csrftoken')
+                    }
+                })
+                    .then(resp => resp.json()
+                    )
+                    .then(data => {
+                        setTags(data.tags)
+                    });
+            }}
+        >
 
-    <input className="" type="file" name="document" style={{outline: "1px solid #afafaf", width: "50%"}} accept="image/*"></input>
+            {
+                !formState.upload &&
+                <input className="" type="file" name="document" style={{ outline: "1px solid #afafaf", width: "50%" }} accept="image/*"></input>
+            }
 
-    <div className="alert alert-success mt-4">
-      <p>A navbar was detected in the doodle. Enter a title for the Navbar: </p>
-      <input type="text" name="navbarTitle"></input>
-    </div>
-
-
-
-    <div className="alert alert-success mt-4 form-group d-inline-block">
-      <p>A carousel was detected in the doodle. Select the number of images in the carousel: </p>
-      <select name="carousel" className="form-control d-inline-block w-50" >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-      </select>
-    </div>
-
-
-
-    {/* <button className="btn btn-dark ml-4 px-4" type="submit">Add customisation</button> */}
-
-
-    <div className="d-inline-block mt-3 alert alert-primary w-50">
-      <input type="checkbox" name="darkMode" value='true' className="checkbox"></input>
-      <label className="ml-2 align-top d-inline" htmlFor="darkMode">Dark Mode (Adds dark theme to the generated html)</label>
-    </div>
-    <input className="btn btn-dark ml-4 px-4" type="submit"></input>
+            {
+                formState.navbar &&
+                <div className="alert alert-success mt-4">
+                    <p>A navbar was detected in the doodle. Enter a title for the Navbar: </p>
+                    <input type="text" name="navbarTitle"></input>
+                </div>
+            }
 
 
-  </form>
+            {
+                formState.carousel &&
+                <div className="alert alert-success mt-4 form-group d-inline-block">
+                    <p>A carousel was detected in the doodle. Select the number of images in the carousel: </p>
+                    <select name="carousel" className="form-control d-inline-block w-50" >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select>
+                </div>
+            }
+
+            {
+                formState.upload && 
+                <button className="btn btn-dark ml-4 px-4" type="submit">Add customisation</button>
+            }
+
+
+            <div className="d-inline-block mt-3 alert alert-primary w-50">
+                <input type="checkbox" name="darkMode" value='true' className="checkbox"></input>
+                <label className="ml-2 align-top d-inline" htmlFor="darkMode">Dark Mode (Adds dark theme to the generated html)</label>
+            </div>
+
+            {
+                !formState.upload &&
+                <input className="btn btn-dark ml-4 px-4" type="submit"></input>
+            }
+
+
+        </form>
     );
-}
-
-const submitHandler = async (e) => {
-    e.preventDefault();
-    const form = document.getElementById('form');
-    const data = new FormData(form);
-    fetch('/readImage/', {
-       method: 'POST',
-       body: data,
-       headers: {
-           "X-CSRFToken": getCoockie('csrftoken')
-       }
-   })
-   .then(resp => {
-       console.log(resp.json())
-       console.log(resp.text())
-     }
-       )
-//    .then(data => {
-//        console.log(data);
-//    });
 }
 
 const getCoockie = (name) => {
